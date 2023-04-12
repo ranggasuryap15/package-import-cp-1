aeb590bf7015446e8804194b10f2a4e91da7ad3b
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-04-13 06:33:23 +0700

    init exercise

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func AdvanceCalculator(calculate string) float32 {
+	return 0 // TODO: replace this
+}
+
+func main() {
+	res := AdvanceCalculator("3 * 4 / 2 + 10 - 5")
+
+	fmt.Println(res)
+}
